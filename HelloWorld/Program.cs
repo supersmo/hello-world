aae6d00efd49e96258f7cf2a5a0eeeb797a2788c
Samuel Mörling <samuel.morling@seb.se>
2019-02-15 11:01:29 +0100

    changed to dotnet core

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,16 +1,12 @@
-﻿// A Hello World! program in C#.
-using System;
+﻿using System;
+
 namespace HelloWorld
 {
-    class Hello
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
